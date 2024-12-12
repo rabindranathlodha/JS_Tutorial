1171f34e0f7ed8e5b6a18d5f9850220d495683f1
Rabindranath Lodha <rabilodha789@gmail.com>
2024-12-12 14:37:31 +0530

    varriable learned

---
+++ 01_Basics/01_varriable.js
@@ -0,0 +1,13 @@
+const accountID = 1223345   //fixed value cannot be changed
+let accountEmail = "rabi@google.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+//accountID = 13325//not allowed
+accountEmail = "newrabi@google.com"
+accountpassword = "4782154871"
+accountCity = "delhi"
+
+console.log(accountID);
+console.table([accountEmail, accountpassword, accountCity, accountState])
